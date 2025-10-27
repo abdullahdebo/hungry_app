b582a7d6bab5f9ef3421c3fc58ccd2367eea05ff
A. DEBO <abd.valancia2@gmail.com>
2025-10-27 03:31:55 +0300

    Add ProfileView, CartView, HomeView, and OrderHistoryView; implement Root with navigation

---
+++ lib/root.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hungry_app/core/constants/app_colors.dart';
+import 'package:hungry_app/features/auth/view/profile_view.dart';
+import 'package:hungry_app/features/cart/view/cart_view.dart';
+import 'package:hungry_app/features/home/view/home_view.dart';
+import 'package:hungry_app/features/orderHistory/view/order_history_view.dart';
+
+class Root extends StatefulWidget {
+  const Root({super.key});
+
+  @override
+  State<Root> createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  late PageController controller;
+  late List<Widget> screens;
+  int currentScreen = 0;
+
+  @override
+  void initState() {
+    screens = [
+      HomeView(),
+      CartView(),
+      OrderHistoryView(),
+      ProfileView(),
+    ];
+    controller = PageController(initialPage: currentScreen);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(controller: controller, children: screens),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: AppColors.primaryColor,
+          borderRadius: BorderRadius.circular(17),
+        ),
+        padding: EdgeInsets.all(10),
+        child: BottomNavigationBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey.shade600,
+          currentIndex: currentScreen,
+          onTap: (index) {
+            setState(() {
+              currentScreen = index;
+            });
+            controller.jumpToPage(index);
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.cart),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_restaurant_sharp),
+              label: 'Order History',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.profile_circled),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
