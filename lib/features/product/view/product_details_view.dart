a8094ea9103f3cc5d4ad28e0d3386da65fef5fd0
A. DEBO <abd.valancia2@gmail.com>
2025-11-05 05:32:17 +0300

    Refactor ProductDetailsView layout; streamline ToppingCard display and adjust spacing for improved UI

--- lib/features/product/view/product_details_view.dart
+++ lib/features/product/view/product_details_view.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:hungry_app/core/constants/app_colors.dart';
 import 'package:hungry_app/features/product/widget/spicy_slider.dart';
 import 'package:hungry_app/features/product/widget/topping_card.dart';
 import 'package:hungry_app/shared/custom_button.dart';
@@ -31,106 +30,102 @@ class _ProductDetailsViewState
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 15),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SpicySlider(
-                value: value,
-                onChanged: (v) {
-                  setState(() => value = v);
-                  print(value);
-                },
-              ),
-              Gap(50),
-              CustomText(text: 'Toppings', size: 20),
-              Gap(20),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    ToppingCard(
-                      imageUrl: 'assets/details/tomato.png',
-                      name: 'Tomato',
-                      onAdd: () => print('Added Tomato'),
-                    ),
-                    Gap(12),
-                    ToppingCard(
-                      imageUrl: 'assets/details/onion.png',
-                      name: 'Onion',
-                      onAdd: () => print('Added Onion'),
-                    ),
-                    Gap(12),
-                    ToppingCard(
-                      imageUrl: 'assets/details/pickles.png',
-                      name: 'Pickles',
-                      onAdd: () => print('Added Pickles'),
-                    ),
-                    Gap(12),
-                    ToppingCard(
-                      imageUrl: 'assets/details/bacons.png',
-                      name: 'Bacons',
-                      onAdd: () => print('Added Bacons'),
-                    ),
-                  ],
-                ),
-              ),
-              Gap(35),
-              CustomText(text: 'Toppings', size: 20),
-              Gap(20),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    ToppingCard(
-                      imageUrl: 'assets/details/fries.png',
-                      name: 'Fries',
-                      onAdd: () => print('Added Fries'),
-                    ),
-                    Gap(12),
-                    ToppingCard(
-                      imageUrl: 'assets/details/coleslaw.png',
-                      name: 'Coleslaw',
-                      onAdd: () => print('Added Coleslaw'),
-                    ),
-                    Gap(12),
-                    ToppingCard(
-                      imageUrl: 'assets/details/salad.png',
-                      name: 'Salad',
-                      onAdd: () => print('Added Salad'),
-                    ),
-                    Gap(12),
-                    ToppingCard(
-                      imageUrl: 'assets/details/onionring.png',
-                      name: 'Onions',
-                      onAdd: () => print('Onions Ring'),
-                    ),
-                  ],
-                ),
-              ),
-              Gap(60),
-              Row(
-                mainAxisAlignment:
-                    MainAxisAlignment.spaceBetween,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SpicySlider(
+              value: value,
+              onChanged: (v) {
+                setState(() => value = v);
+                print(value);
+              },
+            ),
+            Gap(20),
+            CustomText(text: 'Toppings', size: 20),
+            Gap(20),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CustomText(text: 'Total', size: 20),
-                      CustomText(text: '\$18.9', size: 20),
-                    ],
+                  ToppingCard(
+                    imageUrl: 'assets/details/tomato.png',
+                    name: 'Tomato',
+                    onAdd: () => print('Added Tomato'),
                   ),
-                  CustomButton(
-                    text: 'Add To Cart',
-                    onTap: () {
-                      print('Add To Cart Clicked !');
-                    },
+                  Gap(12),
+                  ToppingCard(
+                    imageUrl: 'assets/details/onion.png',
+                    name: 'Onion',
+                    onAdd: () => print('Added Onion'),
+                  ),
+                  Gap(12),
+                  ToppingCard(
+                    imageUrl: 'assets/details/pickles.png',
+                    name: 'Pickles',
+                    onAdd: () => print('Added Pickles'),
+                  ),
+                  Gap(12),
+                  ToppingCard(
+                    imageUrl: 'assets/details/bacons.png',
+                    name: 'Bacons',
+                    onAdd: () => print('Added Bacons'),
                   ),
                 ],
               ),
-              Gap(150),
-            ],
-          ),
+            ),
+            Gap(35),
+            CustomText(text: 'Side Options', size: 20),
+            Gap(20),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ToppingCard(
+                    imageUrl: 'assets/details/fries.png',
+                    name: 'Fries',
+                    onAdd: () => print('Added Fries'),
+                  ),
+                  Gap(12),
+                  ToppingCard(
+                    imageUrl: 'assets/details/coleslaw.png',
+                    name: 'Coleslaw',
+                    onAdd: () => print('Added Coleslaw'),
+                  ),
+                  Gap(12),
+                  ToppingCard(
+                    imageUrl: 'assets/details/salad.png',
+                    name: 'Salad',
+                    onAdd: () => print('Added Salad'),
+                  ),
+                  Gap(12),
+                  ToppingCard(
+                    imageUrl: 'assets/details/onionring.png',
+                    name: 'Onions',
+                    onAdd: () => print('Onions Ring'),
+                  ),
+                ],
+              ),
+            ),
+            Gap(60),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomText(text: 'Total', size: 16),
+                    CustomText(text: '\$18.9', size: 20),
+                  ],
+                ),
+                CustomButton(
+                  text: 'Add To Cart',
+                  onTap: () {
+                    print('Add To Cart Clicked !');
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
