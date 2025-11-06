576ecbb1af715ec4a0552c9fc0ba3c35fa658f99
A. DEBO <abd.valancia2@gmail.com>
2025-11-06 04:08:56 +0300

    Refactor OrderHistoryView to display order details in a list format; implement custom card layout with image, quantity, and price information.

--- lib/features/orderHistory/view/order_history_view.dart
+++ lib/features/orderHistory/view/order_history_view.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:hungry_app/shared/custom_button.dart';
+import 'package:hungry_app/shared/custom_text.dart';
 
 class OrderHistoryView extends StatelessWidget {
   const OrderHistoryView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Order History')));
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0,
+        scrolledUnderElevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: ListView.builder(
+          padding: EdgeInsets.only(top: 20, bottom: 120),
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment:
+                          MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset(
+                          'assets/splash/test.png',
+                          width: 125,
+                        ),
+                        Column(
+                          crossAxisAlignment:
+                              CrossAxisAlignment.start,
+                          children: [
+                            CustomText(
+                              text: 'Humurger',
+                              weight: FontWeight.bold,
+                            ),
+                            CustomText(text: 'Qty : X3'),
+                            CustomText(text: 'Price : 20\$'),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Gap(20),
+                    CustomButton(
+                      color: Color(0xff3A3A3A),
+                      text: 'Order Again',
+                      width: double.infinity,
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
